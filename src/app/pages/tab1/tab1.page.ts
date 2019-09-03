6ad006734095f6f6ffe077453d78ca6912f601ce
Manel Bermudez <manel.bermudez@credimarket.com>
2019-09-03 16:15:07 +0200

    añadir items en lista

---
+++ src/app/pages/tab1/tab1.page.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { DeseosService } from '../../services/deseos.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor(public deseosService: DeseosService, private router: Router, private alertCtrl: AlertController) {
+
+  }
+
+  async agregarLista() {
+
+    const alert = await this.alertCtrl.create({
+      header: 'Nueva lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('cancelar');
+          }
+        },
+        {
+          text: 'Crear',
+          handler: ( data ) => {
+            if ( data.titulo.length === 0 ) {
+              return;
+            }  else {
+              // create list
+              const listId = this.deseosService.crearLista( data.titulo );
+
+              this.router.navigateByUrl(`/tabs/tab1/agregar/${ listId }`);
+            }
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
+
+}
